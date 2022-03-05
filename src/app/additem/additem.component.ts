0ddbb9ce8673cfee05f1e344e493f2c44a78ad0f
Ashish Musani <ashishmusani@Ashishs-MacBook-Air.local>
2022-03-05 00:09:16 -0500

    Added alerts on add item screen

--- src/app/additem/additem.component.ts
+++ src/app/additem/additem.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core'
 import { Router } from '@angular/router';
 import { FireserviceService } from 'src/services/fireservice.service';
 import {Location} from '@angular/common';
+import { ToastController } from '@ionic/angular';
+import { AlertService } from 'src/services/alert-service.service';
 
 @Component({
   selector: 'app-additem',
@@ -14,7 +16,9 @@ export class AddItemComponent implements OnInit {
   public ingredients: String;
   public description: string;
 
-  constructor(private router: Router, public fireService: FireserviceService, private _location: Location) { }
+  constructor(private router: Router, public fireService: FireserviceService, 
+              private _location: Location, public toastController: ToastController,
+              private alertService: AlertService) { }
 
   ngOnInit() {}
 
@@ -25,9 +29,18 @@ export class AddItemComponent implements OnInit {
   addItem() {
     this.fireService.addItem({itemname: this.itemname, ingredients: this.ingredients, description: this.description}).then(res => {
       this._location.back();
+      this.presentToast();
       }, err => {
-        alert(err);
+        this.alertService.genericAlert("Error", "There was a problem. Please try again.")
       })
+    }
+
+    async presentToast() {
+      const toast = await this.toastController.create({
+        message: 'Item added successfully',
+        duration: 2000
+      });
+      toast.present();
     }
 
 }
